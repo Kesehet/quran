3dfb5e953c6c48a8734fa0a2b15fb74bf4038dfe
Kesehet <hamood.siddiqui@gmail.com>
2023-12-04 16:54:47 +0530

    optimizer updated ...

--- optimize.py
+++ optimize.py
@@ -1,24 +1,17 @@
 from PIL import Image
 import os
-import requests
-from io import BytesIO
 
-# Function to optimize an image
-def optimize_image(image_path, output_path, size=(800, 800), quality=85):
+# Function to optimize and convert an image to WebP
+def optimize_and_convert_to_webp(image_path, output_path, size=(800, 800), quality=90):
     with Image.open(image_path) as img:
-        img.thumbnail(size)
-        img.save(output_path, quality=quality)
-
-# Function to convert an image to WebP
-def convert_to_webp(image_path, output_path, quality=90):
-    with Image.open(image_path) as img:
-        img.save(output_path, format='webp', quality=quality)
-
-
+        img.thumbnail(size)  # Resize the image
+        # Save the image in WebP format
+        webp_output_path = os.path.splitext(output_path)[0] + '.webp'
+        img.save(webp_output_path, format='webp', quality=quality)
 
 # Directory containing images
 image_dir = "tajweed-quran"
-output_dir = "optimize-tajweed-quran"
+output_dir = "optimized-webp-tajweed-quran"
 
 # Create output directory if it doesn't exist
 if not os.path.exists(output_dir):
@@ -29,4 +22,4 @@ for filename in os.listdir(image_dir):
     if filename.lower().endswith(('.png', '.jpg', '.jpeg')):
         image_path = os.path.join(image_dir, filename)
         output_path = os.path.join(output_dir, filename)
-        optimize_image(image_path, output_path)
+        optimize_and_convert_to_webp(image_path, output_path)
